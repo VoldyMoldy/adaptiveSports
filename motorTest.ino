8e9fbcec60751cca97ffebc10fa0c1b6b292fe68
VoldyMoldy <59066649+VoldyMoldy@users.noreply.github.com>
2022-07-27 19:13:37 -0400

    working swing code

--- motorTest.ino
+++ motorTest.ino
@@ -4,9 +4,6 @@ unsigned long currentTime = 0;
 unsigned long periodStart = 0;
 unsigned long pulseTime = 1650;
 int buttonState = 0;
-bool buttonPressed = false;
-int buttonPressedTime = 0;
-bool ignoreButton = false;
 void setup() {
   // put your setup code here, to run once:
   currentTime = micros();
@@ -21,27 +18,5 @@ void loop() {
   // put your main code here, to run repeatedly:
   currentTime = micros();
   buttonState = digitalRead(buttonPin);
-  if(buttonState == HIGH && ignoreButton == false) {
-    if(not buttonPressed) {
-      buttonPressedTime = currentTime;
-      buttonPressed = true;
-    } if((currentTime - periodStart) >= 50000) {
-      digitalWrite(motorPin, HIGH);
-      periodStart = currentTime;
-    } if((currentTime - periodStart) >= pulseTime) {
-      digitalWrite(motorPin, LOW);
-    } if((currentTime - buttonPressedTime) >= 2000000) {
-      ignoreButton = true;
-      buttonPressedTime = currentTime;
-    }
-  } if(ignoreButton) {
-    if((currentTime - buttonPressedTime) >= 1500000) {
-      ignoreButton = false;
-      buttonPressedTime = 0;
-    } if((currentTime - periodStart) >= 50000) {
-      digitalWrite(motorPin, HIGH);
-    } if((currentTime - periodStart) >= 1250) {
-      digitalWrite(motorPin, LOW);
-    }
-  }
+  Serial.print(buttonState);
 }
